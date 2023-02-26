c622a6ba0ca1d8d0f1b8ea1ddc15afee78c5e637
Chibuzor Obiora <cobiora@securitycompass.com>
2023-02-26 17:40:43 -0500

    rest api - business domain grouping-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
-import { CoffessService } from './coffess/coffess.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
   imports: [CoffeesModule],
-  controllers: [AppController, CoffeesController],
-  providers: [AppService, CoffessService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
