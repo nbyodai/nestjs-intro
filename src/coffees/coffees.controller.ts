bfae1f04ff53960d1b1a94e7e4986e7e6dfeb09b
Chibuzor Obiora <cobiora@securitycompass.com>
2023-02-26 14:05:13 -0500

    rest api-route-params-1

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll() {
     return 'this action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param() params) {
+    return `this action returns ${params.id} coffee`;
   }
 }
