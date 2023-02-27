c8359328f2fd9a93c9b27e509e523b64b1e98cea
Chibuzor Obiora <cobiora@securitycompass.com>
2023-02-26 22:37:40 -0500

    chap 3 - use transactions

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesService } from 'src/coffees/coffees.service';
+import { Event } from 'src/events/entities/event.entity';
 import { CoffeesController } from './coffees.controller';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
