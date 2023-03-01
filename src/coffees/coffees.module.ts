214708fd45dd05a37680b6089ad119cc0262f9a3
Chibuzor Obiora <cobiora@securitycompass.com>
2023-03-01 12:00:01 -0500

    C5 - namespaces and partial registration - 1

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,13 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesService } from 'src/coffees/coffees.service';
 import { Event } from 'src/events/entities/event.entity';
+import coffeesConfig from './coffees.config';
 import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event]), ConfigModule],
+  imports: [
+    TypeOrmModule.forFeature([Coffee, Flavor, Event]),
+    ConfigModule.forFeature(coffeesConfig),
+  ],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
