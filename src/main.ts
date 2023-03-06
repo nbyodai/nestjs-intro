ebd701d2fb850e819853fdc1f99d91bf123c1860
Chibuzor Obiora <cobiora@securitycompass.com>
2023-03-06 14:07:21 -0500

    C6 - interceptors  - 2

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ApiKeyGuard } from './common/guards/api-key.guard';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
@@ -17,9 +18,10 @@ async function bootstrap() {
       },
     }),
   );
-  app.useGlobalInterceptors(new WrapResponseInterceptor());
-  // app.useGlobalFilters(new HttpExceptionFilter());
-  // app.useGlobalGuards(new ApiKeyGuard());
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
   await app.listen(4000);
 }
 bootstrap();
