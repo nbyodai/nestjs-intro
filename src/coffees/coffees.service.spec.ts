b8fd74d9f053ba1f1f78b2242bb382b35b6ca0ff
Chibuzor Obiora <cobiora@securitycompass.com>
2023-03-27 12:32:17 -0400

    setup service with mock injected

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+import { PrismaService } from '../prisma-service.service';
+import coffeesConfig from './coffees.config';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: DataSource, useValue: {} },
+        {
+          provide: getRepositoryToken(Coffee),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Flavor),
+          useValue: {},
+        },
+        { provide: COFFEE_BRANDS, useValue: [] },
+        { provide: coffeesConfig.KEY, useValue: {} },
+        { provide: PrismaService, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
